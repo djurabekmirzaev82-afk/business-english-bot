06fb223a0535e6e8eb31a50d195967036cb4ee96
djurabekmirzaev82-afk <djurabekmirzaev82@gmail.com>
2026-08-12 16:56:10 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -6,5 +6,5 @@ router.use('/auth', require('./routes/auth'));
 router.use('/progress', require('./routes/progress'));
 router.use('/writing', require('./routes/writing'));
 router.use('/content', require('./routes/content'));
-
+router.use('/speaking', require('./routes/speaking'));
 module.exports = router;
